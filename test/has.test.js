78c31782a562e767e20682f6b1da2aa722550d29
Coco Guerra <jorgeguerra1098@gmail.com>
2020-08-27 15:13:00 -0600

    Update has.test.js

--- test/has.test.js
+++ test/has.test.js
@@ -3,4 +3,5 @@ const { has } = require('../werkstatt');
 test('if the second argument exists either as value in an array or as prop in a json', () => {
     expect(has([3, 5], 3)).toEqual(true);
     expect(has(["Hola", "adios"], "true")).toEqual(false);
+    expect(has({ name: "Jorge" }, "name")).toEqual(true);
 });
