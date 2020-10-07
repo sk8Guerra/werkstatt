b6be181541b24c89e13d86fcfdb0307e20655d66
Coco Guerra <jorgeguerra1098@gmail.com>
2020-10-07 17:49:37 -0600

    build(index.d.ts): support typescript types

--- src/float/roundUp.js
+++ src/float/roundUp.js
@@ -1,5 +1,12 @@
-function roundUp(number) {
-  return Math.ceil(number);
+/**
+ * Round up the argument to next closest integer.
+ *
+ * @param {number} value The value to round up.
+ * @returns {number} The value rounded up.
+ */
+
+function roundUp(value) {
+  return Math.ceil(value);
 }
 
 export default roundUp;
