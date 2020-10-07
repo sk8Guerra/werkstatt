5ba72d602470188530067a9e958b4da570f7fa79
Coco Guerra <jorgeguerra1098@gmail.com>
2020-10-07 17:59:03 -0600

    refactor(rownddown.js): support jsdoc to work with ts types

--- src/float/roundDown.js
+++ src/float/roundDown.js
@@ -1,3 +1,10 @@
+/**
+ * Round down the argument to the previous integer.
+ *
+ * @param {number} value The value to round down.
+ * @returns {number} The value rounded down.
+ */
+
 function roundDown(number) {
   return Math.floor(number);
 }
