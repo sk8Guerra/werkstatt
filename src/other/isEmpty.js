a57b5df087b44be7ca10249711f920f74bfa5125
Coco Guerra <jorgeguerra1098@gmail.com>
2020-10-07 18:09:08 -0600

    refactor(isempty.js): support jsdoc for ts types in isempry

--- src/other/isEmpty.js
+++ src/other/isEmpty.js
@@ -1,5 +1,12 @@
 import isLengthOf from '../array/isLengthOf';
 
+/**
+ * Check if the passed argument is empty.
+ *
+ * @param {any} param The param to check.
+ * @returns {boolean} true: if the value is empty. false: if the value is not empty. 
+ */
+
 const isEmpty = param => {
   if (typeof(param) === 'string') {
     return isLengthOf(param.trim(), 0);
