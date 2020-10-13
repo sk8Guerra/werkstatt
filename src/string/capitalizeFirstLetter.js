5682d86f1e6ec8efd466421e6f37ea5b5ece695f
Coco Guerra <jorgeguerra1098@gmail.com>
2020-10-13 10:57:09 -0600

    docs(capitalizefirstletter.js isemail.js): support jsdoc to match with ts types

--- src/string/capitalizeFirstLetter.js
+++ src/string/capitalizeFirstLetter.js
@@ -1,6 +1,14 @@
-function capitalizeFirstLetter(value) {
-  if (typeof value !== 'string') return '';
-  return value.charAt(0).toUpperCase() + value.slice(1);
+
+/**
+ * Capitalize the first letter of a string.
+ *
+ * @param {string} string The string to capitalize (only the first letter).
+ * @returns {string} The string with only the first letter capitalized.
+ */
+
+function capitalizeFirstLetter(string) {
+  if (typeof string !== 'string') return '';
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
 export default capitalizeFirstLetter;
