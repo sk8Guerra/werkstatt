7ee400153119bbd10c9eb6f09cc698d45c6af76d
Coco Guerra <jorgeguerra1098@gmail.com>
2021-04-04 17:50:09 -0600

    test(subtract.js): add more test cases para subtract

--- test/number/subtract.test.js
+++ test/number/subtract.test.js
@@ -3,4 +3,6 @@ const { subtract } = require('../../dist');
 test('subtract two numbers', () => {
     expect(subtract(6, 3)).toBe(3);
     expect(subtract({}, 3)).toBe(0);
+    expect(subtract(false, 3)).toBe(0);
+    expect(subtract(undefined, 3)).toBe(0);
 });
