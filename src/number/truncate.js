19d1a2d2c789dfafedf662de9848de09662b4989
Coco Guerra <jorgeguerra1098@gmail.com>
2021-04-04 18:20:53 -0600

    fix(truncate.js): handle undefined values

--- src/number/truncate.js
+++ src/number/truncate.js
@@ -1,3 +1,5 @@
+import isDefined from "../other/isDefined";
+import isNumber from "./isNumber";
 
 /**
  * Returns the integer part of a number by removing any fractional digits.
@@ -7,7 +9,11 @@
  */
 
 const truncate = (value) => {
-  return Math.trunc(value)
+  if(isDefined(value) && isNumber(value)) {
+    return Math.trunc(value) ;
+  }
+
+  return 0;
 }
 
 export default truncate;
--- test/number/truncate.test.js
+++ test/number/truncate.test.js
@@ -1,6 +1,7 @@
 const { truncate } = require('../../dist');
 
 test('Returns the integer part of a number by removing any fractional digits.', () => {
-    const truncated = truncate(123.4567);
-    expect(truncated).toBe(123);
+    expect(truncate(123.4567)).toBe(123);
+    expect(truncate(undefined)).toBe(0);
+    expect(truncate(null)).toBe(0);
 });
