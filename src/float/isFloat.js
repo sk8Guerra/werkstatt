3dd657c78ec603526e3d1cfb46f4bb4382ef3fd5
Coco Guerra <jorgeguerra1098@gmail.com>
2021-04-06 10:01:47 -0600

    fix(isfloat.js): handle posible exceptions

--- src/float/isFloat.js
+++ src/float/isFloat.js
@@ -1,3 +1,4 @@
+import isNumber from "../number/isNumber";
 
 /**
  * Check whether the passed argument is a float number or not.
@@ -5,9 +6,12 @@
  * @param {any} value The value to check.
  * @returns {boolean} true: if the value is a float number. false: if the value is not a float number.
  */
+const isFloat = (value) => {
+  if (isNumber(value)) {
+    return value % 1 !== 0
+  }
 
-function isFloat(value) {
-  return value % 1 !== 0
+  return false;
 }
 
 export default isFloat;
--- test/float/isFloat.test.js
+++ test/float/isFloat.test.js
@@ -4,4 +4,11 @@ test('if the passed argument is float or not', () => {
     expect(isFloat(3)).toEqual(false);
     expect(isFloat(0.54)).toEqual(true);
     expect(isFloat(3.54)).toEqual(true);
+
+    expect(isFloat([])).toBe(false);
+    expect(isFloat({})).toBe(false);
+    expect(isFloat(NaN)).toBe(false);
+    expect(isFloat(undefined)).toBe(false);
+    expect(isFloat(null)).toBe(false);
+    expect(isFloat("null")).toBe(false);
 });
