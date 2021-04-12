0660204603b498a9f3927068a22002d50e3b72e3
Coco Guerra <jorgeguerra1098@gmail.com>
2021-04-12 10:00:19 -0600

    fix(isempty): handle possible exceltions

--- src/other/isEmpty.js
+++ src/other/isEmpty.js
@@ -1,4 +1,6 @@
 import isLengthOf from '../array/isLengthOf';
+import isUndefined from './isUndefined';
+import isNull from "./isNull";
 
 /**
  * Check whether the passed value is empty or not.
@@ -7,7 +9,9 @@ import isLengthOf from '../array/isLengthOf';
  * @returns {boolean} true: if the value is empty. false: if the value is not empty. 
  */
 
-const isEmpty = param => {
+const isEmpty = param => {cle
+  if(isUndefined(param) || isNull(param)) return true;
+
   if (typeof(param) === 'string') {
     return isLengthOf(param.trim(), 0);
   }
--- test/other/isEmpty.test.js
+++ test/other/isEmpty.test.js
@@ -3,4 +3,8 @@ const { isEmpty } = require('../../dist');
 test('if the passed argument is undefined', () => {
     expect(isEmpty({})).toEqual(true);
     expect(isEmpty([])).toEqual(true);
+    expect(isEmpty(null)).toEqual(true);
+    expect(isEmpty(undefined)).toEqual(true);
+    expect(isEmpty('')).toEqual(true);
+    expect(isEmpty('Hallo')).toEqual(false);
 });
